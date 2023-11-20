3bbdbea16ddb13c6849c39cec36cad26937db974
Hugo Porchet <hugo.porchet.belmonte@gmail.com>
2023-11-20 10:33:26 +0100

    feat: walk over the folder with ignore for git support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+
+use clap::Parser;
+use ignore::Walk;
+use log::debug;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to scan dir
+    #[arg(value_name = "DIR")]
+    path: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Args::parse();
+
+    let path = if let Some(p) = args.path {
+        p
+    } else {
+        debug!("chemin non passé en paramétre");
+        env::current_dir()
+            .unwrap()
+            .to_str()
+            .expect("Pas de chemin valide trouvé pour le répertoire courant")
+            .to_string()
+    };
+
+    debug!("chemin à chercher {}", path);
+    for r in Walk::new(path) {
+        match r {
+            Ok(entry) => {
+                if entry.clone().path().is_dir() {
+                    println!("{}", entry.path().display());
+                }
+            }
+            Err(err) => debug!("{}", err),
+        }
+    }
 }
